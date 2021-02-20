86757fbf74cfaf95a505ed988548c6c30806efd0
aminin55 <43518781+aminin55@users.noreply.github.com>
2021-02-20 19:40:01 +0300

    Add files via upload

--- bestsum.cpp
+++ bestsum.cpp
@@ -1,4 +1,3 @@
-#include <fstream>
 #include <iostream>
 
 using namespace std;
@@ -6,24 +5,33 @@ using namespace std;
 int main()
 {
 int n;
-int ibest=0,jbest=1,imin=0;
-cin >> n;
-int values[n];
+int imin = 0;
+int ibest = 0;
+int jbest = 1;
 
-for (int i = 0; i<n; i++){
-    cin >> values[i];
+cin >> n;
+int a[n];
+for (int i=0;i<n;i++){
+    cin>>a[i];
+}
+
+
+    for (int j = 2; j < n; ++j)
+    {
+        if (a[j - 1] < a[imin])
+            imin =  j - 1;
+        if (a[j] / a[imin] > a[jbest] / a[ibest])
+        {
+            jbest = j;
+            ibest = imin;
+        }
     }
 
-for (int j=2; j<n; ++j){
-    if( values[j-1] <values[imin])
-        imin=j-1;
-    if(values[j]-values[imin]>values[jbest]-values[ibest])
-        ibest=imin,jbest=j;
-
-}
-    if(values[jbest]/values[ibest]>1)
-        cout << ibest <<" "<<jbest<<endl;
+    if(a[jbest]/a[ibest]>1)
+        cout << ibest+1 <<" "<<jbest+1<<endl;
     else
         cout << "0 0";
+
     return 0;
 }
+
